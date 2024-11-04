2ff2a1305710a440a4f06447631821732deeeaba
Vadim Kostrov <vadimkostrov070@gmail.com>
2024-11-04 17:06:14 +0300

    Create method to autogenerate input fields from Object file

--- src/components/InputSetup.jsx
+++ src/components/InputSetup.jsx
@@ -1,19 +1,21 @@
-import React, { useRef, useState } from "react";
+import React from "react";
 import { inputStructure } from "../assets/inputStructure";
 
 export default function InputSetup(props) {
-  const section = inputStructure[props.section].title;
+  const SectionObject = inputStructure[props.section];
+  const Title = SectionObject.title;
+  const Fields = SectionObject.fields;
+
+  const keyArrayList = Object.keys(Fields).map((key) => key);
 
   return (
-    <div>
-      {/* Inputfield */}
-      {[
-        "Personal Information",
-        "Professional Summary",
-        "Work Experience",
-        "Education Certifications",
-        "Skills",
-      ].map((item) => {})}
+    <div className="flex flex-col">
+      {keyArrayList.map((field) => (
+        <div className="flex w-max flex-col" key={field}>
+          <label>{Fields[field].label}</label>
+          <input type={Fields[field].type} className="border"></input>
+        </div>
+      ))}
     </div>
   );
 }
