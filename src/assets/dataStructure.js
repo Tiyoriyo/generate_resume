ba82a4f55332db4e35f8697459405c192420d07a
Vadim Kostrov <vadimkostrov070@gmail.com>
2024-11-07 21:05:31 +0300

    Add a keep data state controller

---
+++ src/assets/dataStructure.js
@@ -0,0 +1,66 @@
+export const dataStructure = {
+  personalInformation: {
+    fullName: {
+      value: undefined,
+    },
+    professionalTitle: {
+      value: undefined,
+    },
+    phoneNumber: {
+      value: undefined,
+    },
+    email: {
+      value: undefined,
+    },
+    linkedInProfile: {
+      value: undefined,
+    },
+    portfolio: {
+      value: undefined,
+    },
+  },
+  professionalSummary: {
+    summary: {
+      value: undefined,
+    },
+  },
+  workExperience: {
+    jobTitle: {
+      value: undefined,
+    },
+    companyName: {
+      value: undefined,
+    },
+    location: {
+      value: undefined,
+    },
+    startDate: {
+      value: undefined,
+    },
+    endDate: {
+      value: undefined,
+    },
+    responsibilities: {
+      value: undefined,
+    },
+  },
+  educationCertifications: {
+    degree: {
+      value: undefined,
+    },
+    institution: {
+      value: undefined,
+    },
+    graduationDate: {
+      value: undefined,
+    },
+    achievements: {
+      value: undefined,
+    },
+  },
+  skills: {
+    skill: {
+      value: undefined,
+    },
+  },
+};
--- src/components/InputSetup.jsx
+++ src/components/InputSetup.jsx
@@ -1,21 +1,98 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { inputStructure } from "../assets/inputStructure";
+import { data } from "../assets/dataHolder";
+
+const initialState = {
+  personalInformation: {
+    fullName: {
+      value: undefined,
+    },
+    professionalTitle: {
+      value: undefined,
+    },
+    phoneNumber: {
+      value: undefined,
+    },
+    email: {
+      value: undefined,
+    },
+    linkedInProfile: {
+      value: undefined,
+    },
+    portfolio: {
+      value: undefined,
+    },
+  },
+  professionalSummary: {
+    summary: {
+      value: undefined,
+    },
+  },
+  workExperience: {
+    jobTitle: {
+      value: undefined,
+    },
+    companyName: {
+      value: undefined,
+    },
+    location: {
+      value: undefined,
+    },
+    startDate: {
+      value: undefined,
+    },
+    endDate: {
+      value: undefined,
+    },
+    responsibilities: {
+      value: undefined,
+    },
+  },
+  educationCertifications: {
+    degree: {
+      value: undefined,
+    },
+    institution: {
+      value: undefined,
+    },
+    graduationDate: {
+      value: undefined,
+    },
+    achievements: {
+      value: undefined,
+    },
+  },
+  skills: {
+    skill: {
+      value: undefined,
+    },
+  },
+};
 
 export default function InputSetup(props) {
-  const SectionObject = inputStructure[props.section];
-  const Fields = SectionObject.fields;
-  const keyArrayList = Object.keys(Fields).map((key) => key);
-  const sectionHandler = props.setSectionHandler;
+  const [tempData, setTempData] = useState(initialState);
+
+  const fieldList = inputStructure[props.section].fields;
+  const keys = Object.keys(fieldList).map((key) => key);
 
   return (
     <div className="flex w-full flex-col gap-4 border p-8 shadow-md">
       <div className="flex flex-col gap-2">
-        {keyArrayList.map((field) => (
+        {keys.map((field, i) => (
           <div className="flex w-full flex-col" key={field}>
-            <label>{Fields[field].label}</label>
+            <label>{fieldList[field].label}</label>
             <input
-              type={Fields[field].type}
+              type={fieldList[field].type}
               className="h-8 border shadow-sm"
+              onChange={(e) => {
+                const fieldName = Object.keys(fieldList)[i];
+                const inputData = e.target.value;
+                let temp = tempData;
+                temp[props.section][fieldName].value = inputData;
+                console.log(tempData);
+                setTempData(temp);
+              }}
+              value={tempData[props.section][Object.keys(fieldList)[i]].value}
             ></input>
           </div>
         ))}
@@ -25,7 +102,9 @@ export default function InputSetup(props) {
           className="w-16 border bg-green-300"
           onClick={() => {
             if (props.sectionIndex < 4) {
-              sectionHandler(props.sectionIndex + 1);
+              data[props.section] = props.setSectionHandler(
+                props.sectionIndex + 1,
+              );
             }
           }}
         >
